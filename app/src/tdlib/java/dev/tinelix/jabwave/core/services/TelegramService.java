1eb7e6455587d65b641938b7178f6715d0d361d7
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2024-03-13 18:16:24 +0700

    Update TelegramService.java

--- app/src/tdlib/java/dev/tinelix/jabwave/core/services/TelegramService.java
+++ app/src/tdlib/java/dev/tinelix/jabwave/core/services/TelegramService.java
@@ -1,9 +1,12 @@
 package dev.tinelix.jabwave.core.services;
 
 import android.annotation.SuppressLint;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -12,6 +15,7 @@ import org.drinkless.td.libcore.telegram.TdApi;
 import java.util.HashMap;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import dev.tinelix.jabwave.JabwaveApp;
 import dev.tinelix.jabwave.api.base.BaseClient;
 import dev.tinelix.jabwave.core.services.base.ClientService;
@@ -22,7 +26,7 @@ import dev.tinelix.jabwave.api.tdlwrap.entities.Authenticator;
 import dev.tinelix.jabwave.api.tdlwrap.entities.Chat;
 import dev.tinelix.jabwave.api.tdlwrap.models.Chats;
 import dev.tinelix.jabwave.api.tdlwrap.models.Services;
-import dev.tinelix.jabwave.api.base.SecureStorage;
+import dev.tinelix.jabwave.net.telegram.api.SecureStorage;
 import dev.tinelix.jabwave.ui.enums.HandlerMessages;
 
 /**
@@ -54,10 +58,24 @@ public class TelegramService extends ClientService implements TDLibClient.ApiHan
         this.ctx = ctx;
         phone_number = map.get("username");
         if(status.equals("done") || isConnected()) {
+            AlarmManager mgr = (AlarmManager) ctx.getSystemService(Context.ALARM_SERVICE);
             intent = new Intent(ctx, TelegramService.class);
             intent.setAction(ACTION_START);
             intent.putExtra(PHONE_NUMBER, phone_number);
-            ctx.startService(intent);
+            PendingIntent pendingIntent = null;
+            // Setting PendingIntent for Android API Level 23 and above
+            if (Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
+                pendingIntent = PendingIntent.getService(this, 0, intent,
+                        PendingIntent.FLAG_MUTABLE);
+            } else if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                pendingIntent = PendingIntent.getService(this, 0, intent,
+                        PendingIntent.FLAG_IMMUTABLE);
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                ctx.startForegroundService(intent);
+            } else {
+                ctx.startService(intent);
+            }
             Log.d(JabwaveApp.TELEGRAM_SERV_TAG, "Service started.");
             ctx.bindService(intent, connection, BIND_AUTO_CREATE);
         } else {
@@ -81,6 +99,11 @@ public class TelegramService extends ClientService implements TDLibClient.ApiHan
         }
         intent.setAction(ACTION_STOP);
         stopService(intent);
+    }
+
+    @Override
+    public int onStartCommand(@Nullable Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
@@ -152,33 +175,24 @@ public class TelegramService extends ClientService implements TDLibClient.ApiHan
     private void sendMessageToActivity(String status) {
         Intent intent = new Intent();
         switch (status) {
-            case "required_phone_number":
-                intent.putExtra("msg", HandlerMessages.AUTHENTICATION_ERROR);
-                break;
-            case "error":
-                intent.putExtra("msg", HandlerMessages.NO_INTERNET_CONNECTION);
-                break;
-            case "required_auth_code":
-                intent.putExtra("msg", HandlerMessages.REQUIRED_AUTH_CODE);
-                break;
-            case "required_cloud_password":
-                intent.putExtra("msg", HandlerMessages.REQUIRED_CLOUD_PASSWORD);
-                break;
-            case "authorized":
-                intent.putExtra("msg", HandlerMessages.AUTHORIZED);
-                break;
-            case "account_loaded":
-                intent.putExtra("msg", HandlerMessages.ACCOUNT_LOADED);
-                break;
-            case "update_chat_status":
-                intent.putExtra("msg", HandlerMessages.CHATS_UPDATED);
-                break;
-            case "done":
-                intent.putExtra("msg", HandlerMessages.DONE);
-                break;
-            default:
-                intent.putExtra("msg", HandlerMessages.UNKNOWN_ERROR);
-                break;
+            case "required_phone_number" ->
+                    intent.putExtra("msg", HandlerMessages.AUTHENTICATION_ERROR);
+            case "error" ->
+                    intent.putExtra("msg", HandlerMessages.NO_INTERNET_CONNECTION);
+            case "required_auth_code" ->
+                    intent.putExtra("msg", HandlerMessages.REQUIRED_AUTH_CODE);
+            case "required_cloud_password" ->
+                    intent.putExtra("msg", HandlerMessages.REQUIRED_CLOUD_PASSWORD);
+            case "authorized" ->
+                    intent.putExtra("msg", HandlerMessages.AUTHORIZED);
+            case "account_loaded" ->
+                    intent.putExtra("msg", HandlerMessages.ACCOUNT_LOADED);
+            case "update_chat_status" ->
+                    intent.putExtra("msg", HandlerMessages.CHATS_UPDATED);
+            case "done" ->
+                    intent.putExtra("msg", HandlerMessages.DONE);
+            default ->
+                    intent.putExtra("msg", HandlerMessages.UNKNOWN_ERROR);
         }
         intent.setAction("dev.tinelix.jabwave.TELEGRAM_RECEIVE");
         sendBroadcast(intent);
@@ -187,21 +201,16 @@ public class TelegramService extends ClientService implements TDLibClient.ApiHan
     private void sendMessageToActivity(String status, Bundle data) {
         Intent intent = new Intent();
         switch (status) {
-            case "required_auth_code":
-                intent.putExtra("msg", HandlerMessages.REQUIRED_AUTH_CODE);
-                break;
-            case "required_cloud_password":
-                intent.putExtra("msg", HandlerMessages.REQUIRED_CLOUD_PASSWORD);
-                break;
-            case "auth_error":
-                intent.putExtra("msg", HandlerMessages.AUTHENTICATION_ERROR);
-                break;
-            case "update_file":
-                intent.putExtra("msg", HandlerMessages.FILES_UPDATED);
-                break;
-            case "presence_changed":
-                intent.putExtra("msg", HandlerMessages.CHATS_LOADED);
-                break;
+            case "required_auth_code" ->
+                    intent.putExtra("msg", HandlerMessages.REQUIRED_AUTH_CODE);
+            case "required_cloud_password" ->
+                    intent.putExtra("msg", HandlerMessages.REQUIRED_CLOUD_PASSWORD);
+            case "auth_error" ->
+                    intent.putExtra("msg", HandlerMessages.AUTHENTICATION_ERROR);
+            case "update_file" ->
+                    intent.putExtra("msg", HandlerMessages.FILES_UPDATED);
+            case "presence_changed" ->
+                    intent.putExtra("msg", HandlerMessages.CHATS_LOADED);
         }
         intent.putExtra("data", data);
         intent.setAction("dev.tinelix.jabwave.TELEGRAM_RECEIVE");
@@ -257,43 +266,42 @@ public class TelegramService extends ClientService implements TDLibClient.ApiHan
                     ((TdApi.UpdateAuthorizationState) object).authorizationState;
             Log.d(JabwaveApp.APP_TAG, String.format("Updating authorization state to %s...", state.getClass().getSimpleName()));
             switch (state.getConstructor()) {
-                case TdApi.AuthorizationStateWaitPhoneNumber.CONSTRUCTOR:
-                    status = "auth_error";
-                    break;
-                case TdApi.AuthorizationStateWaitCode.CONSTRUCTOR:
-                    status = "required_auth_code";
-                    break;
-                case TdApi.AuthorizationStateWaitPassword.CONSTRUCTOR:
-                    status = "required_cloud_password";
-                    break;
-                case TdApi.AuthorizationStateReady.CONSTRUCTOR:
+                case TdApi.AuthorizationStateWaitPhoneNumber.CONSTRUCTOR ->
+                        status = "auth_error";
+                case TdApi.AuthorizationStateWaitCode.CONSTRUCTOR ->
+                        status = "required_auth_code";
+                case TdApi.AuthorizationStateWaitPassword.CONSTRUCTOR ->
+                        status = "required_cloud_password";
+                case TdApi.AuthorizationStateReady.CONSTRUCTOR -> {
                     ((Authenticator) auth).setAuthState(state);
                     status = "authorized";
-                    break;
-                default:
-                    break;
+                }
+                default -> {
+                }
             }
             sendMessageToActivity(status);
         } else if(object instanceof TdApi.UpdateUserStatus) {
             TdApi.UpdateUserStatus userStatus = ((TdApi.UpdateUserStatus) object);
             Chat chat = null;
-            switch(userStatus.status.getConstructor()) {
-                case TdApi.UserStatusOnline.CONSTRUCTOR:
-                    chat = (Chat) getChats().getChatById(userStatus.userId);
-                    if(chat != null)
-                        chat.status = 1;
-                    break;
-                case TdApi.UserStatusOffline.CONSTRUCTOR:
-                    chat = (Chat) getChats().getChatById(userStatus.userId);
-                    if(chat != null)
-                        chat.status = 0;
-                    break;
-            }
-            if(chat != null) {
-                if (getChats().getChatIndex(chat) >= 0) {
-                    getChats().chats.set(getChats().getChatIndex(chat), chat);
-                    status = "update_chat_status";
-                    sendMessageToActivity(status);
+            if(getChats().getChatById(userStatus.userId) instanceof Chat) {
+                switch (userStatus.status.getConstructor()) {
+                    case TdApi.UserStatusOnline.CONSTRUCTOR -> {
+                        chat = (Chat) getChats().getChatById(userStatus.userId);
+                        if (chat != null)
+                            chat.status = 1;
+                    }
+                    case TdApi.UserStatusOffline.CONSTRUCTOR -> {
+                        chat = (Chat) getChats().getChatById(userStatus.userId);
+                        if (chat != null)
+                            chat.status = 0;
+                    }
+                }
+                if (chat != null) {
+                    if (getChats().getChatIndex(chat) >= 0) {
+                        getChats().chats.set(getChats().getChatIndex(chat), chat);
+                        status = "update_chat_status";
+                        sendMessageToActivity(status);
+                    }
                 }
             }
         } else if(object instanceof TdApi.UpdateFile) {
