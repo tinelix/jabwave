3460e0d788aece1b9a9f8aa002e749999e021bc1
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2024-03-15 16:04:21 +0700

    Update AccountSettingsFragment.java

--- app/src/main/java/dev/tinelix/jabwave/core/fragments/settings/AccountSettingsFragment.java
+++ app/src/main/java/dev/tinelix/jabwave/core/fragments/settings/AccountSettingsFragment.java
@@ -8,6 +8,7 @@ import androidx.annotation.Nullable;
 import androidx.preference.Preference;
 import dev.tinelix.jabwave.R;
 import dev.tinelix.jabwave.core.activities.SettingsActivity;
+import dev.tinelix.jabwave.core.services.base.ClientService;
 import dev.tinelix.jabwave.core.utilities.FragmentNavigator;
 
 public class AccountSettingsFragment extends PreferenceFragmentCompat {
@@ -21,15 +22,18 @@ public class AccountSettingsFragment extends PreferenceFragmentCompat {
     private void listenPreferences() {
         Preference aboutAccountPref = findPreference("about_account");
         if(getActivity() instanceof SettingsActivity activity) {
-            boolean isAuthenticated = activity.service.getAuthenticator().isAuthenticated();
-            if (aboutAccountPref != null) {
-                if(isAuthenticated) {
-                    boolean isChangeable = activity.service.getAuthenticator().isChangeableAuthData();
-                    String not_supported = getResources().getString(R.string.not_supported_by_protocol);
-                    aboutAccountPref.setEnabled(isChangeable);
-                    aboutAccountPref.setSummary(isChangeable ? null : not_supported);
-                } else {
-                    aboutAccountPref.setVisible(false);
+            ClientService service = activity.service;
+            if(service != null) {
+                boolean isAuthenticated = service.getAuthenticator().isAuthenticated();
+                if (aboutAccountPref != null) {
+                    if (isAuthenticated) {
+                        boolean isChangeable = activity.service.getAuthenticator().isChangeableAuthData();
+                        String not_supported = getResources().getString(R.string.not_supported_by_protocol);
+                        aboutAccountPref.setEnabled(isChangeable);
+                        aboutAccountPref.setSummary(isChangeable ? null : not_supported);
+                    } else {
+                        aboutAccountPref.setVisible(false);
+                    }
                 }
             }
         }
